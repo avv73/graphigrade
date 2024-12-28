9e92cb200804539607f97f9b8bce0f70a48090b7
Aleksandar Valov <28270014+avv73@users.noreply.github.com>
2024-12-28 16:10:48 +0200

    fix(style): Omit html tag.

--- src/Services/Identity/EmailSender.cs
+++ src/Services/Identity/EmailSender.cs
@@ -23,14 +23,12 @@ public class EmailSender : IEmailSender
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
         _logger.LogGeneralDebug(DateTime.UtcNow, ServiceName, nameof(SendEmailAsync), $"Send Email Call received! - {email} {subject} {htmlMessage}");
-
-        htmlMessage = $"<html>{htmlMessage}</html>";
-
+        
         bool result = await _mailgunApiClient.SendMailAsync(
             SenderEmail, 
             email, 
             subject,
-            textContent: string.Empty,
+            textContent:string.Empty,
             htmlContent:htmlMessage);
 
         _logger.LogGeneralDebug(DateTime.UtcNow, ServiceName, nameof(SendEmailAsync), $"Send Email Call Result: {result}");
