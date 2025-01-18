3b1a314fd3fd1b676be79c3bb2c87912d798d12d
Aleksandar Valov <28270014+avv73@users.noreply.github.com>
2025-01-18 15:14:33 +0200

    feat(user): Add faculty number to user. (#11)
    * feat(user): Add faculty number to user.
    
    * fix(html): Fix autocomplete field.

---
+++ src/Data/Migrations/20250118122007_AddFacultyNumber.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphiGrade.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFacultyNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FacultyNumber",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacultyNumber",
+                schema: "identity",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/Models/Identity/User.cs
+++ src/Models/Identity/User.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace GraphiGrade.Models.Identity;
 
 public class User : IdentityUser<int>
 {
     public ICollection<UserGroup>? UserGroups { get; set; }
+
     public ICollection<UserGroup>? ManagesGroups { get; set; }
+
+    [MaxLength(10)]
+    public required string FacultyNumber { get; set; }
 }
